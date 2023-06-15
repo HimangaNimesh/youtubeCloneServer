321d6ece064a9665b0f49be0ba635835a8090e71
himanganimesh2 <himanganimesh2@gmail.com>
2023-06-16 00:06:19 +0530

    routes and controller files are created

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import UserRoutes from "./server/User/User.Routes.js"
+import VideoRoutes from "./server/Video/Video.Routes.js"
+import CommentRoutes from "./server/Comment/Comment.Routes.js"
+import authRoutes from "./server/auth/auth.Routes.js"
 
 const app =express()
 dotenv.config()
@@ -13,6 +17,11 @@ const connect = () => {
         throw error
     })
 }
+
+app.use("/api/auth", authRoutes)
+app.use("/api/users", UserRoutes)
+app.use("/api/videos", VideoRoutes)
+app.use("/api/comments", CommentRoutes)
 
 app.listen(8000, ()=>{
     connect()
---
+++ server/Comment/Comment.Routes.js
@@ -0,0 +1,6 @@
+import express from "express"
+import { } from "./Comment.Controller.js";
+
+const router = express.Router()
+
+export default router;

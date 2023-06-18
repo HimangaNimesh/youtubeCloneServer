db662e3db0dfd857de793c3e8a9f03088b64da79
himanganimesh2 <himanganimesh2@gmail.com>
2023-06-18 11:08:28 +0530

    comments getting and delete are done

--- server/Comment/Comment.Controller.js
+++ server/Comment/Comment.Controller.js
@@ -1,4 +1,6 @@
 import CommentModel from "./Comment.Model.js"
+import VideoModel from "../Video/Video.Model.js"
+import { createError } from "../../error.js"
 
 export const addComment = async(req, res, next) => {
     const newComment = new CommentModel({...req.body, userId:req.user.id})
@@ -12,7 +14,15 @@ export const addComment = async(req, res, next) => {
 
 export const deleteComment = async(req, res, next) => {
     try {
-        
+        const comment = await CommentModel.findById(res.params.id)
+        const video = await VideoModel.findById(res.params.id) 
+        if(req.user.id === comment.userId || req.user.id === video.userId){
+            await CommentModel.findByIdAndDelete(req.params.id)
+            res.status(200).json("Comment has been deleted!")
+        }
+        else{
+            return next(createError(403, "You can only delete your comments!"))
+        }
     } catch (error) {
         next(error)
     }
@@ -20,7 +30,8 @@ export const deleteComment = async(req, res, next) => {
 
 export const getComments = async(req, res, next) => {
     try {
-        
+        const comments = await CommentModel.find({videoId: req.params.videoId})
+        res.status(200).json(comments)
     } catch (error) {
         next(error)
     }
